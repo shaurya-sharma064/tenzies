1cf17dd0ce027d9f450083509555b3a196feaf8d
Shaurya <shaurya064@gmail.com>
2022-02-10 01:28:10 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import Die from "./components/Die"
 import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
 import Clock from "./images/clock.png"
+import Dice from "./images/dice.png"
+import Reset from "./images/reset.png"
+import Leaderboard from "./images/leaderboard.png"
+import Back from "./images/back.png"
 
 function App(){
 
     const [dice,setDice]=React.useState(allRoller())
     const [tenzies,setTenzies]=React.useState(false)
     const [timer,setTimer]=React.useState(0)
+    const [rolls,setRolls]=React.useState(0)
+    const [screen,setScreen]=React.useState(true)
+    const [topScore,setTopScore]=React.useState(()=>JSON.parse(localStorage.getItem("topScore"))|| [])
+    const [staticTimer,setStaticTimer]=React.useState(0)
 
-    React.useEffect(()=>setTimeout(()=>setTimer(prevTimer=>prevTimer+1),1000),[timer])
+    
+    React.useEffect(()=>timeInterval(),[])
+
+    
+
 
     React.useEffect(()=>{
         const isHeld=dice.every(die=>die.isHeld)
@@ -21,6 +33,58 @@ function App(){
         }
     },[dice])
 
+    React.useEffect(()=>{
+        if(tenzies){
+            setStaticTimer(timer)
+            setTopScore(prevScores=>{        
+                if(prevScores.length===0){
+                    return [{rolls:rolls,timer:timer,score:1000-(timer+rolls)}]
+                }else{
+                    let i
+                    let flag=0
+                    let newScores=[]
+                    for(i=0;i<prevScores.length;i++){
+                        if(prevScores[i].score<1000-(timer+rolls) && flag===0){
+                            newScores.push({rolls:rolls,timer:timer,score:1000-(timer+rolls)})
+                            flag=1
+                        }
+                        if(newScores.length<10){
+                            newScores.push(prevScores[i])
+                        }
+                    }
+                    if(flag!=1 && newScores.length<10){
+                        newScores.push({rolls:rolls,timer:timer,score:1000-(timer+rolls)})
+                    }
+                    return newScores
+                }
+            })
+        }
+    },[tenzies])
+
+    React.useEffect(()=>{
+        localStorage.setItem("topScore",JSON.stringify(topScore))
+    },[topScore])
+
+    function tableData(){
+        let newArr=[]
+        for(let i=0;i<topScore.length;i++){
+            newArr.push(<tr key={i+1}>
+                <td>
+                    {i+1}
+                </td>
+                <td>
+                    {topScore[i].rolls}
+                </td>
+                <td>
+                    {topScore[i].timer}
+                </td>
+                <td>
+                    {topScore[i].score}
+                </td>
+            </tr>)
+        }
+        return newArr
+    }
 
 
     function allRoller(){
@@ -32,10 +96,26 @@ function App(){
     }
 
     function notHeldRoller(){
+        setRolls(prevRoll=>prevRoll+1)
         setDice((prevDice)=>{
             return prevDice.map(die=>die.isHeld?die:{...die,value:singleRoller()})
         })
         setTenzies(false)
+    }
+    
+
+    function timeInterval(){
+        setInterval(()=>setTimer(prevTimer=>prevTimer+1),1000)
+    }
+
+    function resetGame(){
+        setDice(allRoller)
+        setTenzies(false)
+        setRolls(0)
+        setTimer(0)
+        setScreen(true)
+        
+        
     }
 
     function singleRoller(){
@@ -51,21 +131,67 @@ function App(){
             return prevDice.map(die=>die.id===id?{...die,isHeld:!die.isHeld}:die)
         })
     }
-    
-
+    if(screen){
     return (
         <div>
+            {tenzies && <Confetti />}
             <main>
-                {tenzies && <Confetti />}
                 
-                <h1 id="heading">Tenzies {timer}</h1>
+                <div id="options">
+                    <img id="img-reset" src={Reset} onClick={resetGame}/><img id="img-leaderboard" src={Leaderboard} onClick={()=>setScreen(false)}/>
+                    
+                </div>
+                <h1 id="heading">Tenzies</h1>
+
                 <p id="para">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+
+                <div id="scores">
+    
+                    <div id="rolls">
+                        <p id="heading--rolls">Rolls</p>
+                        <img id="logo--roll" src={Dice}/>
+                        <p id="currentScore">{rolls}</p>
+                    </div>
+
+                    <div id="timer">
+                        <p id="heading--timer">Timer</p>
+                        <img id="logo--timer" src={Clock}/>
+                        <p id="currentTimer">{tenzies?staticTimer:timer}</p>
+                    </div>
+
+                </div>
+                
                 <div className="dieHolder">
                     {tenDice()}
                 </div>
-                <button className="btn" onClick={tenzies?()=>{setDice(allRoller)
-                    setTenzies(false)
-                    }:notHeldRoller}>{tenzies?"New Game":"Click Here"}</button>
+                <button className="btn" onClick={tenzies?resetGame:notHeldRoller}>{tenzies?"New Game":"Roll Dice"}</button>
+            </main>
+        </div>
+    )
+    }
+    return( 
+        <div>
+            <main>
+            <div id="options">
+                    <img id="img-back" src={Back} onClick={resetGame}/>
+            </div>
+            <h1 id="heading">Leaderboard</h1>
+                {topScore.length===0 && <h1>No rankings yet</h1>}
+                {topScore.length!==0 && <table>
+                    <thead>
+                        <tr>
+                            <th>Rank</th>
+                            <th>Rolls</th>
+                            <th>Time</th>
+                            <th>Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tableData()}
+                    </tbody>
+                    </table>}
+            <table>
+            </table>
             </main>
         </div>
     )
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,12 +1,38 @@
+import One from "../images/one.png"
+import Two from "../images/two.png"
+import Three from "../images/three.png"
+import Four from "../images/four.png"
+import Five from "../images/five.png"
+import Six from "../images/six.png"
 
 export default function Die(props){
     const styler={
         backgroundColor:props.isHeld?"#0088DD":"#fff"
     }
-
+    let value
+    switch(props.val){
+        case 1:
+            value=One
+            break
+        case 2:
+            value=Two
+            break
+        case 3:
+            value=Three
+            break
+        case 4:
+            value=Four
+            break
+        case 5:
+            value=Five
+            break
+        case 6:
+            value=Six
+            break
+    }
     return(
         <div className="die" onClick={props.switcher} style={styler}>
-            {props.val}
+            <img src={value} alt={props.val}/>
         </div>
     )
 }
